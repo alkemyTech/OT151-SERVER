c86652282c2f8659c052f210caa7a080effa8814
Fernando Ciballero <fernando.ciballero@rootstrap.com>
2022-02-17 17:52:19 -0300

    add test model Person

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_02_14_011556) do
+ActiveRecord::Schema.define(version: 2022_02_17_204851) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
