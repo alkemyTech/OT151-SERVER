a5c309923d73d6027fbe2ea3de7c7070e43200f6
chocolatito <usuariodetelegram@gmail.com>
2022-02-18 21:19:02 -0300

    feat OT151-19: add model migration and controller for announcement

---
+++ app/models/announcement.rb
@@ -0,0 +1,25 @@
+# == Schema Information
+#
+# Table name: announcements
+#
+#  id           :bigint           not null, primary key
+#  content      :text             not null
+#  discarded_at :datetime
+#  image_url    :string           not null
+#  name         :string           not null
+#  created_at   :datetime         not null
+#  updated_at   :datetime         not null
+#
+# Indexes
+#
+#  index_announcements_on_discarded_at  (discarded_at)
+#
+class Announcement < ApplicationRecord
+  include Discard::Model
+  validates :name, presence: true, length: { minimum: 3 }
+  validates :content, presence: true, length: { minimum: 3 }
+  validates :image_url, presence: true,
+                        length: { minimum: 3 }
+  # :image_url does not allow blank spaces, in any position of the string
+  validates :image_url, format: { without: /\s/, message: 'must contain no spaces' }
+end
