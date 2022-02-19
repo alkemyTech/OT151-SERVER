989705aabed5dda73a2e05f1bc76d6aa56d51035
lautarograc <lautarograciani2106@gmail.com>
2022-02-19 00:40:25 -0300

    Added model, migration and base controller for User

---
+++ db/migrate/20220219033219_create_users.rb
@@ -0,0 +1,15 @@
+class CreateUsers < ActiveRecord::Migration[6.1]
+  def change
+    create_table :users do |t|
+      t.string :email, unique: true, null: false
+      t.string :first_name, null: false
+      t.string :last_name, null: false
+      t.string :password, null: false
+      t.string :photo
+      t.datetime :discarded_at
+      t.index :discarded_at
+      t.timestamps
+    end
+    end
+  end
+
--- db/schema.rb
+++ db/schema.rb
@@ -10,16 +10,21 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_02_14_011556) do
+ActiveRecord::Schema.define(version: 2022_02_19_033219) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
-  create_table "people", force: :cascade do |t|
-    t.string "name"
-    t.string "address"
+  create_table "users", force: :cascade do |t|
+    t.string "email", null: false
+    t.string "first_name", null: false
+    t.string "last_name", null: false
+    t.string "password", null: false
+    t.string "photo"
+    t.datetime "discarded_at"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.index ["discarded_at"], name: "index_users_on_discarded_at"
   end
 
 end
