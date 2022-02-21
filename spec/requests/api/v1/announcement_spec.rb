841477865d48101d32888fcf04efb3f1fa7eaa9e
chocolatito <usuariodetelegram@gmail.com>
2022-02-21 16:49:22 -0300

    feat OT151-19: add model migration and controller for announcement

--- spec/requests/api/v1/announcement_spec.rb
+++ spec/requests/api/v1/announcement_spec.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
-# require 'rails_helper'
+require 'rails_helper'
 
-# RSpec.describe "/api/v1/Announcements", type: :request do
-# end
+RSpec.describe '/api/v1/Announcements', type: :request do
+  pending "add some examples to (or delete) #{__FILE__}"
+end
